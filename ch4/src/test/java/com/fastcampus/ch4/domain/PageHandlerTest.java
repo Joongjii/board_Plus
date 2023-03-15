50f03ac9fa3767710d0b262c261c243aed62ac92
Joongjii <guswnddl1217@gmail.com>
2023-03-15 23:56:21 +0900

    log

---
+++ ch4/src/test/java/com/fastcampus/ch4/domain/PageHandlerTest.java
@@ -0,0 +1,14 @@
+package com.fastcampus.ch4.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PageHandlerTest {
+
+    @Test
+    public void test(){
+        PageHandler ph = new PageHandler(250, 1);
+    }
+
+}
